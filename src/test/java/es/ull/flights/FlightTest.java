ee2f17558ffc93573a78686ab7b3ac3ada84ab7e
lauramanzini <232566@studenti.unimore.it>
2021-12-12 12:01:35 +0000

    Initial commit

---
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -0,0 +1,30 @@
+package es.ull.flights;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FlightTest {
+    @Test
+    void testAddPassenger() {
+
+    }
+
+    @Test
+    void testGetFlightNumber() {
+
+    }
+
+    @Test
+    void testGetNumberOfPassengers() {
+
+    }
+
+    @Test
+    void testRemovePassenger() {
+
+    }
+}
---
+++ src/test/java/es/ull/passengers/PassengerTest.java
@@ -0,0 +1,45 @@
+package es.ull.passengers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PassengerTest {
+    @Test
+    void testGetCountryCode() {
+        System.out.println("This is a try");
+    }
+
+    @Test
+    void testGetFlight() {
+
+    }
+
+    @Test
+    void testGetIdentifier() {
+
+    }
+
+    @Test
+    void testGetName() {
+
+    }
+
+    @Test
+    void testJoinFlight() {
+
+    }
+
+    @Test
+    void testSetFlight() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
